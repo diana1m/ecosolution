e60dce6a0e7ae5fff3c1c425e5ae2a739f96afa7
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-12-01 18:06:36 +0200

    create section Electrcity

--- src/components/Electricity/Electricity.tsx
+++ src/components/Electricity/Electricity.tsx
@@ -3,8 +3,15 @@ import { oswaldFont } from '../../app/fonts'
 
 const Electricity = () => {
   return (
-    <section id='electricity' className={styles.section}>
-      <h2 className={`${oswaldFont.className} ${styles.title}`}>Electricity</h2>
+    <section
+      id='electricity'
+      className={`${oswaldFont.className} ${styles.section}`}
+    >
+      <h2 className={styles.title}>Electricity we produced for all time</h2>
+      <div className={styles.numberWrapper}>
+        <span className={styles.number}>1.134.147.814</span>
+        <span className={styles.text}>kWh</span>
+      </div>
     </section>
   )
 }
